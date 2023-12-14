8fc6814c32d3f7487ce0d932f5ecf9b5cf129350
Michnowicz <michnowicz@outlook.com>
2023-12-14 23:04:41 +0100

    basic rpc

--- js/main.js
+++ js/main.js
@@ -1,33 +1,78 @@
 
-let choice = ["pierre", "papier", "ciseaux"]
-let playerPoints = 0
-let aiPoints = 0
-let round = 0
+let choice = ["rock", "paper", "cisors"]
+let playerFinalScore = 0
+let pcFinalScore = 0
+let run = prompt("do you want to play? (y?n)")
 
+while (run != n) {
+    let playerPoints = 0
+    let pcPoints = 0
+    let round = 0
+    let win = false
 
-function rpc() {
-}
-
-
-// choose a random option for the computer
-function ai() {
-    let choice = ["rock", "paper", "cisors"]
-    return (choice[Math.floor(Math.random()*choice.length)])
-}
-
-
-
-
-
-while (playerPoints >= 3 || aiPoints >=3 || round >= 5) {
-    let player_choice = prompt("choose your next move (rock, paper, cisors")
-    let ai_choice = ai()
-    console.log(ai_choice)
-
-    switch (player_choice) {
-        case "rock":
-            if (ai_choice == "rock") {
-                console.log("draw")
-            }
+    while (playerPoints < 3 && pcPoints < 3 && round < 6) {
+    
+        let pc_choice = (choice[Math.floor(Math.random()*choice.length)])
+        console.log(pc_choice)
+        let player_choice = prompt("Round " + round + ". Choose your next move (rock, paper, cisors)")
+    
+        switch (player_choice) {
+            case "rock":
+                if (pc_choice == "rock") {
+                    alert("the pc chose rock,it's a draw")
+                } else if (pc_choice == "paper") {
+                    alert("the pc chose paper, you've lost this round.")
+                    pcPoints++
+                } else {
+                    alert("the pc chose cisors, you've won this round.")
+                    playerPoints++
+                }
+                alert("player score: " + playerPoints + " | pc score: " + pcPoints)
+                break;
+            case "paper":
+                if (pc_choice == "rock") {
+                    alert("the pc chose rock,you've won this round.")
+                    playerPoints++
+                } else if (pc_choice == "paper") {
+                    alert("the pc chose paper, it's a draw.")
+                } else {
+                    alert("the pc chose cisors, you've lost this round.")
+                    pcPoints++
+                }
+                alert("player score: " + playerPoints + " | pc score: " + pcPoints)
+                break;
+            case "cisors":
+                if (pc_choice == "rock") {
+                    alert("the pc chose rock,you've lost this round.")
+                    pcPoints++
+                } else if (pc_choice == "paper") {
+                    alert("the pc chose paper, you've won this round.")
+                    playerPoints++
+                } else {
+                    alert("the pc chose cisors, it's a draw.")
+                }
+                alert("player score: " + playerPoints + " | pc score: " + pcPoints)
+                break;
+            default:
+                alert("I don't understand that, try again.")
+        }
+        if (choice.includes(player_choice) == true ) {
+            round++
+        }
     }
+    
+    if (playerPoints >= 3 || playerPoints > pcPoints) {
+        alert("You've won " + playerPoints + " to " + pcPoints + ", nice job.")
+        win=true
+        playerFinalScore++
+    } else if (pcPoints >= 3 || pcPoints > playerPoints) {
+        alert("You've lost " + playerPoints + " to " + pcPoints + ", better luck next time.")
+        pcFinalScore++
+    } else {
+        alert("It's a draw. " + playerPoints + " to " + pcPoints)
+    }
+
+
+    run = prompt("another round? (y?n)")
+    if (run != "y" || run != "y" )
 }
